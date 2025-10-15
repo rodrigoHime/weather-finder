4f5434943f7b714557d6d5d3f99d5a96f047d406
Rodrigo Hime <rodrigo.hime@gmail.com>
2025-10-15 09:45:06 -0300

    Update API file paths and initializer references
    - Renamed `api.rb` to `open_weather_map.rb` and updated corresponding initializer.
    - Renamed `api.rb` to `google_api.rb` and updated corresponding initializer.

--- config/initializers/google_api.rb
+++ config/initializers/google_api.rb
@@ -1,4 +1,4 @@
-require "#{Rails.root}/lib/google_api/api.rb"
+require "#{Rails.root}/lib/google_api/google_api.rb"
 
 GoogleApi.configure do |config|
   config.api_key = Rails.application.credentials.config[:google_api_key]
--- config/initializers/open_weather.rb
+++ config/initializers/open_weather.rb
@@ -1,4 +1,4 @@
-require "#{Rails.root}/lib/open_weather_map/api.rb"
+require "#{Rails.root}/lib/open_weather_map/open_weather_map.rb"
 
 OpenWeatherMap.configure do |config|
   config.api_key = Rails.application.credentials.config[:open_weather_api_key]
